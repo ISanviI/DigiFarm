89c9b76d4a687d95da0839a01dbbbc1117d027f6
Sanvi <sanvi.harnale@gmail.com>
2024-09-29 16:33:36 +0530

    Changed BG Image

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -6,9 +6,7 @@ function NavBar() {
   return (
     <>
     <div className="NavBar" style={{display: 'flex'}}>
-      {/* <Link to="/"><div className='navElement'> Image / Logo </div></Link>
-      <Link to="/plant"> <div className='navElement'> Plants </div> </Link>
-      <Link to="/animal"> <div className='navElement'> Animals </div> </Link> */
+      {
       <ul className="nav nav-tabs">
       <li className="nav-item">
         <Link to="/" className="nav-link" aria-current="page" href="#"> Home </Link>
@@ -20,8 +18,7 @@ function NavBar() {
         <Link to="/animal" className="nav-link" href="#" id="animals"> Animals </Link>
         {/* <Link to="/animal"> <a className="nav-link" href="#" id="animals">Animals</a></Link> */}
       </li>
-
-    </ul>}
+      </ul>}
     </div>
     </>
   )
